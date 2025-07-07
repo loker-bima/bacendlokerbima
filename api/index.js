19c1434988eabaedd81746f03ee76b22116d7ec4
mbojostudio <anharaldevaro789@gmail.com>
2025-07-07 16:36:26 +0800

    Initial commit for Vercel

--- api/index.js
+++ api/index.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-const authRoutes = require('./routes/authRoutes');
-const jobRoutes = require('./routes/jobRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('../routes/authRoutes');
+const jobRoutes = require('../routes/jobRoutes');
+const categoryRoutes = require('../routes/categoryRoutes');
+const userRoutes = require('../routes/userRoutes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
